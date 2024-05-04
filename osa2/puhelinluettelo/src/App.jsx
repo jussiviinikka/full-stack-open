434623f9fb47ec0d8b26fc9cd3450ccf9281c100
Jussi Viinikka <jussi.viinikka@gmail.com>
2024-05-04 19:16:35 +0300

    Part 2: phonebook, step1

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Numbers = ({persons}) => {
+	return (
+		<ul>
+		{persons.map(person =>
+			<li key={person.name}>
+				{person.name}
+			</li>
+		)}
+		</ul>
+	)
+} 
+
+const App = () => {
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas' }
+	]) 
+	const [newName, setNewName] = useState('')
+
+	const addNumber = (event) => {
+		event.preventDefault()
+		const newPerson = {
+			name: newName
+		}
+		setPersons(persons.concat(newPerson))
+	}
+
+	const handleInputChange = (event) => {
+		setNewName(event.target.value)
+	}
+	
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addNumber}>
+				<div>
+					name:
+					<input
+						value={newName}
+						onChange={handleInputChange}
+					/>
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+
+			<Numbers persons={persons}/>
+		</div>
+	)
+
+}
+
+export default App
