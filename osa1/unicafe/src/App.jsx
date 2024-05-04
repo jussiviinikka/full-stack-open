c19049276ffc939c4d2808a8df0b0a1b534ea38a
Jussi Viinikka <jussi.viinikka@gmail.com>
2024-05-04 19:16:34 +0300

    Part 1: Unicafe, step6

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,29 +5,33 @@ const Button = ({onClick, text}) => (
 )
 
 const StatisticLine = ({text, value}) => {
-	return (<>{text}: {value}<br/></>)
+	return (<tr><td>{text}</td><td>{value}</td></tr>)
 }
 
 const Statistics = ({good, neutral, bad}) => {
 
 	const all = () => (good + neutral + bad)
 	const positive_perc = () => (
-		100 * (good / (good + neutral + bad))
+		<>{(100 * (good / (good + neutral + bad))).toFixed(1)} %</>
 	)
 	const average = () => (
-		(good - bad) / all()
+		((good - bad) / all()).toFixed(1)
 	)
 
 	if (all() > 0) {
 		return (
 			<div>
-				<h1>Statistics</h1>
-				<StatisticLine text="good" value={good}/>
-				<StatisticLine text="neutral" value={neutral}/>
-				<StatisticLine text="bad" value={bad}/>
-				<StatisticLine text="all" value={all()}/>
-				<StatisticLine text="average" value={average()}/>
-				<StatisticLine text="positive" value={positive_perc()}/>				
+			<h1>Statistics</h1>
+				<table>
+					<tbody>
+						<StatisticLine text="good" value={good}/>
+						<StatisticLine text="neutral" value={neutral}/>
+						<StatisticLine text="bad" value={bad}/>
+						<StatisticLine text="all" value={all()}/>
+						<StatisticLine text="average" value={average()}/>
+						<StatisticLine text="positive" value={positive_perc()}/>
+					</tbody>
+			</table>
 			</div>
 		)
 	}
