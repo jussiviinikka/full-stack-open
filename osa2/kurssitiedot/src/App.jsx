075d783c44de303cdfa5c6d7de1036189f77e1db
Jussi Viinikka <jussi.viinikka@gmail.com>
2024-05-04 19:16:34 +0300

    Part 2: course information, separate module

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,30 +1,4 @@
-const Part = ({name, exercises}) => <p>{name}: {exercises}</p>
-
-const Content = ({parts}) => {
-	return (
-		<ul>
-		{parts.map(part =>
-			<li key={part.id}>
-				<Part name={part.name} exercises={part.exercises}/>
-			</li>
-		)}
-		</ul>
-	)
-} 
-
-const Header = ({name}) => <h2>{name}</h2>
-
-const total = (parts) => parts.map(part => part.exercises).reduce((a, b) => a + b, 0)
-
-const Course = ({course}) => {
-	return (
-		<div>
-			<Header name={course.name}/>
-			<Content parts={course.parts}/>
-			total of  exercises {total(course.parts)}
-		</div>
-	)
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
